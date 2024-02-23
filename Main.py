aa15ed857a5b5c1632fc3d42fd270d8548d6837c
SP-SuryaDEV <123246343+SP-SuryaDEV@users.noreply.github.com>
2024-02-23 14:52:49 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+from mail import SendEmail
+
+st.title('Bulk Emailer')
+
+file = st.file_uploader('Drop your Email\'s here')
+
+if file:
+    data = pd.read_csv(file)
+    emails = list(data['Email'])
+
+    for email in emails:
+        with SendEmail(email) as user:
+            user.sendMessage('This is a Test')
